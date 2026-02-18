6caf51e974374f186d14a804bde12f414b6a550e
harebuss69 <devteam5@hare-business.com>
2026-02-18 23:42:18 +0800

    adsa

--- components/Hero/Parts/InformationPart.jsx
+++ components/Hero/Parts/InformationPart.jsx
@@ -1,17 +1,14 @@
 import Button from "@/components/Button";
-import CalendarIcon from "../../../app/icons/CalendarIcon";
-import LocationIcon from "../../../app/icons/LocationIcon";
-import PaperclipIcon from "../../../app/icons/PaperclipIcon";
-import CalendarIcon from "../../../icons/CalendarIcon";
-import LocationIcon from "../../../icons/LocationIcon";
-import PaperclipIcon from "../../../icons/PaperclipIcon";
+import Calendar from "@/icons/CalendarIcon";
+import LocationIcon from "@/icons/LocationIcon";
+import Paperclip from "@/icons/PaperclipIcon";
 
 export default function InformationPart() {
   return (
     <div className="flex flex-col items-start gap-8">
       <div>
         <div className="flex items-center gap-2">
-          <CalendarIcon className="h-10 w-10" />
+          <Calendar className="h-10 w-10" />
           <p>28th Febuary, 6PM - Late</p>
         </div>
         <div className="flex items-center gap-2">
@@ -23,7 +20,7 @@ export default function InformationPart() {
         </div>
       </div>
       <Button 
-        icon={<PaperclipIcon className="h-10 w-10 text-white" />}
+        icon={<Paperclip className="h-10 w-10 text-white" />}
         text={"REGISTER / JOIN MEET"}
       />
     </div>
