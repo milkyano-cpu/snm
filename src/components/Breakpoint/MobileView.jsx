258c84d9bfbcec93a70d44dd9d807619585cf43e
Levi Indrajaya Sutantio <alevindrajaya@gmail.com>
2026-02-25 21:02:32 +0700

    feat: add feature section for mobile view

--- src/components/Breakpoint/MobileView.jsx
+++ src/components/Breakpoint/MobileView.jsx
@@ -1,3 +1,4 @@
+import FeatureSectionMobile from "../Feature/FeatureSectionMobile";
 import HeroSectionMobile from "../Hero/HeroSectionMobile";
 import { RegistrationFormV3 } from "../RegistrationForm/RegistrationFormV3";
 
@@ -5,6 +6,7 @@ export default function MobileView() {
   return (
     <div className="md:hidden">
       <HeroSectionMobile />
+      <FeatureSectionMobile />
       <RegistrationFormV3 />
     </div>
   );
---
+++ src/components/Feature/FeatureSectionMobile.jsx
@@ -0,0 +1,48 @@
+import CameraIcon from "@/src/icons/CameraIcon";
+import FoodTruckIcon from "@/src/icons/FoodTruckIcon";
+import MotorCycleIcon from "@/src/icons/MotorcycleIcon";
+import PremiumCarIcon from "@/src/icons/PremiumCarIcon";
+
+export default function FeatureSectionMobile() {
+  return (
+    <section>
+      <div className="fading-border mx-auto flex max-w-84 justify-center">
+        <div className="flex max-w-44 flex-col items-center gap-6.5 py-7.5">
+          <div className="features-mobile flex flex-col items-center gap-3.5">
+            <div>
+              <PremiumCarIcon className="h-9 w-12.5" />
+            </div>
+            <p className="text-center font-medium">
+              PREMIUM CARS + CUSTOM BUILDS
+            </p>
+          </div>
+
+          <div className="features-mobile flex flex-col items-center gap-3.5">
+            <div>
+              <MotorCycleIcon className="h-8.5 w-12" />
+            </div>
+            <p className="text-center font-medium">BIKES & CRUISER CULTURE</p>
+          </div>
+
+          <div className="features-mobile flex flex-col items-center gap-3.5">
+            <div>
+              <CameraIcon className="h-5 w-5" />
+            </div>
+            <p className="text-center font-medium">
+              PROFESSIONAL VIDEO & PHOTOGRAPHY
+            </p>
+          </div>
+
+          <div className="flex flex-col items-center gap-3.5">
+            <div>
+              <FoodTruckIcon className="h-5 w-5" />
+            </div>
+            <p className="text-center font-medium">
+              FOOD TRUCKS & POP UP STORE
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
