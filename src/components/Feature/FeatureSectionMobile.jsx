ce02527f182e08b1bf7934c6b6581a7c8c2d266b
Levi Indrajaya Sutantio <alevindrajaya@gmail.com>
2026-02-26 22:25:45 +0700

    chore: Run formatter and linter

--- src/components/Feature/FeatureSectionMobile.jsx
+++ src/components/Feature/FeatureSectionMobile.jsx
@@ -6,7 +6,7 @@ import PremiumCarIcon from "@/src/icons/PremiumCarIcon";
 export default function FeatureSectionMobile() {
   return (
     <section className="bg-feature-section-mobile py-20">
-      <div className="fading-border bg-[#0000008C] mx-auto flex max-w-84 justify-center">
+      <div className="fading-border mx-auto flex max-w-84 justify-center bg-[#0000008C]">
         <div className="features-mobile flex max-w-44 flex-col items-center gap-6 py-8">
           <div className="flex flex-col items-center gap-3.5">
             <div>
